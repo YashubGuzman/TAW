f2c56a32b152dd1e51a672b5a8bcf97b71bfe7e9
YashubGuzman <55424986+YashubGuzman@users.noreply.github.com>
2020-09-09 15:07:38 -0500

    practica 1

---
+++ MVC/Practica 1/models/model.php
@@ -0,0 +1,8 @@
+<?php
+    //Modelo que permite mostrar el enlace de las paginas con las vistas
+    class EnlacesPaginas{
+        public function enlacesPaginasModel(){
+            
+        }
+    }
+?>
