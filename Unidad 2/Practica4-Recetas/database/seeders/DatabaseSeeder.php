61c90c93727eb8e73770097c5027c5ed7a278176
YashubGuzman <55424986+YashubGuzman@users.noreply.github.com>
2020-11-03 15:08:47 -0600

    Se configuraron datos por defecto a las tablas

--- Unidad 2/Practica4-Recetas/database/seeders/DatabaseSeeder.php
+++ Unidad 2/Practica4-Recetas/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //Llamar seeder de categorias
+        $this->call(CategoriaSeeder::class);
+
+        //Llamar seeder de usuarios
+        $this->call(UsuarioSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
---
+++ Unidad 2/Practica4-Recetas/database/seeders/UsuarioSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Hash;
+use Illuminate\Database\Seeder;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Insertar datos por default a la tabla usuarios
+        DB::table('users')->insert([
+            'name'=>'Yashub',
+            'email'=>'yashubge@gmail.com',
+            'password'=>Hash::make('monodelodo'),
+            'url'=>'www.upv.com',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name'=>'Ailin',
+            'email'=>'ailinge@gmail.com',
+            'password'=>Hash::make('monodelodo'),
+            'url'=>'www.upv.com',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name'=>'Yahir',
+            'email'=>'yahirge@gmail.com',
+            'password'=>Hash::make('monodelodo'),
+            'url'=>'www.upv.com',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s'),
+        ]);
+    }
+}
