244b5b1fbd372417db9f484f5c9eb4619c5196ec
YashubGuzman <1630123@upv.edu.mx>
2020-11-03 00:17:40 -0600

    Se corigió la conexión a la base de datos

--- Unidad 1/MVC/Practica 2/models/conexion.php
+++ Unidad 1/MVC/Practica 2/models/conexion.php
@@ -2,7 +2,7 @@
 
 class Conexion{
     public function conectar(){
-        $link = new PDO("mysql:host=localhost;dbname=practica2_bd",'root','');
+        $link = new PDO("mysql:host=localhost;dbname=practica2_bd",'yashub','guzman123');
         return $link;
     }
     //
--- Unidad 1/MVC/Practica 3/models/conexion.php
+++ Unidad 1/MVC/Practica 3/models/conexion.php
@@ -2,12 +2,12 @@
 
 class Conexion{
     public function conectar(){
-        $link = new PDO("mysql:host=localhost;dbname=practica3_bd",'root','');
+        $link = new PDO("mysql:host=localhost;dbname=practica3_bd",'yashub','guzman123');
         return $link;
     }
     //
     public function seleccion(){
-        $mysqli = new mysqli('localhost', "root", "", "practica3_bd");
+        $mysqli = new mysqli('localhost', "yashub", "guzman123", "practica3_bd");
         return $mysqli;
     }
 }
