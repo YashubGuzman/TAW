bf1caed600033b4c5b1cdb7703b5eb94880fae90
GuZmAn <yashubge@gmail.com>
2020-11-05 21:42:52 -0600

    Al iniciar sesion redirecciona al listado de recetas

--- Unidad 2/Practica4-Recetas/app/Providers/RouteServiceProvider.php
+++ Unidad 2/Practica4-Recetas/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/recetas';
+    public const HOME = '/recetas/listado';
 
     /**
      * The controller namespace for the application.
