828256fad8d7cded146260d7490b2f4ae14074c8
YashubGuzman <55424986+YashubGuzman@users.noreply.github.com>
2020-11-23 12:33:13 -0600

    Pagina de principal

--- Unidad 2/Practica4-Recetas/app/Http/Controllers/HomeController.php
+++ Unidad 2/Practica4-Recetas/app/Http/Controllers/HomeController.php
@@ -21,8 +21,35 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $recetas = DB::table('receta2s')->latest()->limit(4)->get();
+        $ultimas = DB::table('receta2s')
+        ->latest()->limit(4)
+        ->get();
 
-        return view('inicio',compact('recetas'));
+        $mexicanas = DB::table('receta2s')
+        ->latest()->limit(4)
+        ->where('categoria_id', '1')
+        ->get();
+
+        $italianas = DB::table('receta2s')
+        ->latest()->limit(4)
+        ->where('categoria_id', '2')
+        ->get();
+
+        $argentinas = DB::table('receta2s')
+        ->latest()->limit(4)
+        ->where('categoria_id', '3')
+        ->get();
+
+        $postres = DB::table('receta2s')
+        ->latest()->limit(4)
+        ->where('categoria_id', '4')
+        ->get();
+
+        $cortes = DB::table('receta2s')
+        ->latest()->limit(4)
+        ->where('categoria_id', '5')
+        ->get();
+
+        return view('inicio',compact('ultimas','mexicanas','italianas','argentinas','postres','cortes'));
     }
 }
--- Unidad 2/Practica4-Recetas/resources/views/inicio.blade.php
+++ Unidad 2/Practica4-Recetas/resources/views/inicio.blade.php
@@ -14,21 +14,23 @@
 </article>
 @endsection
 
-<div class="container nuevas-recetas">
+<!--Aqui se muestran las ultimas 4 recetas que han sido agregadas al sistema -->
+
+<div class="container ultimas-recetas">
 <h2 class="titulo-categoria mt-5 mb-4">ÚLTIMAS RECETAS</h2>
 
 <div class="row">
-    @foreach($recetas as $receta)
+    @foreach($ultimas as $ultima)
     <div class="col-md-3">
         <div class="card">
-            <img src={{ asset('storage').'/'.$receta->imagen}} class="card-img-top" alt="imagen receta">
+            <img src={{ asset('storage').'/'.$ultima->imagen}} class="card-img-top" alt="imagen receta">
 
             <div class="card-body">
-                <h3>{{Str::ucfirst($receta->receta)}}</h3>
+                <h3>{{Str::ucfirst($ultima->receta)}}</h3>
 
-                <p> {{Str::words(strip_tags($receta->preparacion), 15)}} </p>
+                <p> {{Str::words(strip_tags($ultima->preparacion), 15)}} </p>
 
-                <a href="{{route ('recetas.ver', $receta->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
+                <a href="{{route ('recetas.ver', $ultima->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
 
             </div>
         </div>
@@ -39,34 +41,141 @@
 </div>
 
 
+<!--Aqui se muestran las ultimas 4 recetas de COMIDA MEXICANA que han sido agregadas al sistema -->
 
-
-
-<!--
-<div class="row">
-
-    <div class="col-md-3 bg-white p-10 shadow p-3 mb-5 bg-white rounded">
-
-        <img src={{ asset('storage').'/'.'uploads-recetas\3KTYReGdy59imQFSqWE4ztFpdIWOJLn1adJ4PmQO.jpeg'}} class="w-100">
-
-        <div class="ingredientes">
-            <h2 class="my-2">Titulo</h2>
-            Aqui va el texto
+<div class="container comida-mexicana">
+    <h2 class="titulo-categoria mt-5 mb-4">COMIDA MEXICANA</h2>
+    
+    <div class="row">
+        @foreach($mexicanas as $mexicana)
+        <div class="col-md-3">
+            <div class="card">
+                <img src={{ asset('storage').'/'.$mexicana->imagen}} class="card-img-top" alt="imagen receta">
+    
+                <div class="card-body">
+                    <h3>{{Str::ucfirst($mexicana->receta)}}</h3>
+    
+                    <p> {{Str::words(strip_tags($mexicana->preparacion), 15)}} </p>
+    
+                    <a href="{{route ('recetas.ver', $mexicana->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
+    
+                </div>
+            </div>
+            
         </div>
-        
+        @endforeach
+    </div>
     </div>
 
-    <div class="col-md-3 bg-white p-10 shadow p-3 mb-5 bg-white rounded">
 
-        <img src={{ asset('storage').'/'.'uploads-recetas\3KTYReGdy59imQFSqWE4ztFpdIWOJLn1adJ4PmQO.jpeg'}} class="w-100">
 
-        <div class="ingredientes">
-            <h2 class="my-2">Titulo</h2>
-            Aqui va el texto
+<!--Aqui se muestran las ultimas 4 recetas de COMIDA ITALIANA que han sido agregadas al sistema -->
+
+<div class="container comida-italiana">
+    <h2 class="titulo-categoria mt-5 mb-4">COMIDA ITALIANA</h2>
+    
+    <div class="row">
+        @foreach($italianas as $italiana)
+        <div class="col-md-3">
+            <div class="card">
+                <img src={{ asset('storage').'/'.$italiana->imagen}} class="card-img-top" alt="imagen receta">
+    
+                <div class="card-body">
+                    <h3>{{Str::ucfirst($italiana->receta)}}</h3>
+    
+                    <p> {{Str::words(strip_tags($italiana->preparacion), 15)}} </p>
+    
+                    <a href="{{route ('recetas.ver', $italiana->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
+    
+                </div>
+            </div>
+            
         </div>
-        
+        @endforeach
     </div>
-</div>
--->
+    </div>
+
+
+<!--Aqui se muestran las ultimas 4 recetas de COMIDA ARGENTINA que han sido agregadas al sistema -->
+
+<div class="container comida-argentina">
+    <h2 class="titulo-categoria mt-5 mb-4">COMIDA ARGENTINA</h2>
+    
+    <div class="row">
+        @foreach($argentinas as $argentina)
+        <div class="col-md-3">
+            <div class="card">
+                <img src={{ asset('storage').'/'.$argentina->imagen}} class="card-img-top" alt="imagen receta">
+    
+                <div class="card-body">
+                    <h3>{{Str::ucfirst($argentina->receta)}}</h3>
+    
+                    <p> {{Str::words(strip_tags($argentina->preparacion), 15)}} </p>
+    
+                    <a href="{{route ('recetas.ver', $argentina->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
+    
+                </div>
+            </div>
+            
+        </div>
+        @endforeach
+    </div>
+    </div>
+
+
+    <!--Aqui se muestran las ultimas 4 recetas de POSTRESS que han sido agregadas al sistema -->
+
+<div class="container postres">
+    <h2 class="titulo-categoria mt-5 mb-4">POSTRES</h2>
+    
+    <div class="row">
+        @foreach($postres as $postre)
+        <div class="col-md-3">
+            <div class="card">
+                <img src={{ asset('storage').'/'.$postre->imagen}} class="card-img-top" alt="imagen receta">
+    
+                <div class="card-body">
+                    <h3>{{Str::ucfirst($postre->receta)}}</h3>
+    
+                    <p> {{Str::words(strip_tags($postre->preparacion), 15)}} </p>
+    
+                    <a href="{{route ('recetas.ver', $postre->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
+    
+                </div>
+            </div>
+            
+        </div>
+        @endforeach
+    </div>
+    </div>
+
+
+
+    <!--Aqui se muestran las ultimas 4 recetas de COMIDA CORTES que han sido agregadas al sistema -->
+
+<div class="container cortes">
+    <h2 class="titulo-categoria mt-5 mb-4">CORTES</h2>
+    
+    <div class="row">
+        @foreach($cortes as $corte)
+        <div class="col-md-3">
+            <div class="card">
+                <img src={{ asset('storage').'/'.$corte->imagen}} class="card-img-top" alt="imagen receta">
+    
+                <div class="card-body">
+                    <h3>{{Str::ucfirst($corte->receta)}}</h3>
+    
+                    <p> {{Str::words(strip_tags($corte->preparacion), 15)}} </p>
+    
+                    <a href="{{route ('recetas.ver', $corte->id_receta)}}" class="btn btn-danger mr-2 btn-block" text-white>VER RECETA</a>
+    
+                </div>
+            </div>
+            
+        </div>
+        @endforeach
+    </div>
+    </div>
+
 
 @endsection
